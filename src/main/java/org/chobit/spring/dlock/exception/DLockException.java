919a2b396043be74acf50c4ef0628138a993de2e
zhyea <zhangrui@zhyea.com>
2024-06-19 08:45:36 +0800

    1. spring分布式锁的视实现

--- src/main/java/org/chobit/spring/dlock/exception/DLockException.java
+++ src/main/java/org/chobit/spring/dlock/exception/DLockException.java
@@ -2,16 +2,17 @@ package org.chobit.spring.dlock.exception;
 
 
 /**
+ * 分布式锁相关异常
  *
  * @author rui.zhang
  */
 public class DLockException extends RuntimeException {
 
-    public DLockException() {
-    }
+	public DLockException() {
+	}
 
 
-    public DLockException(String message) {
-        super(message);
-    }
+	public DLockException(String message) {
+		super(message);
+	}
 }
